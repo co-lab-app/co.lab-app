be6a20d5dd933bb531951c9a86a7c1c128edf9ba
Sean Sipus <sean.sipus@gmail.com>
2022-03-21 07:41:49 -0400

    first commit

---
+++ src/components/NavMenu/Navbar.js
@@ -0,0 +1,22 @@
+const Nav = style.Nav`
+bakground: #101522;
+height: 80px;
+display: flex;
+justify-content;
+align-items;
+font-size: 1.2rem;
+position: sticky;
+top: 0;
+z-index: 999;
+
+`
+
+const Navbar = () => {
+return (
+  <>
+  <Nav>
+  
+  </Nav>
+  </>
+  )
+}
